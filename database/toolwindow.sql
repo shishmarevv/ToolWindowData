f3b98517746ec94394c7b11da60d02fb63a715e8
Viktor Shishmarev <shishmarevv@protonmail.com>
2025-10-18 18:36:21 +0200

    :card_file_box:sql update

--- database/toolwindow.sql
+++ database/toolwindow.sql
@@ -1,13 +1,48 @@
-BEGIN;
-
-CREATE TABLE IF NOT EXISTS tickets (
+CREATE TABLE IF NOT EXISTS events (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
     timestamp INTEGER NOT NULL,
-    status TEXT NOT NULL CHECK(status IN ('opened', 'closed')),
-    source TEXT CHECK(source IN ('auto', 'manual', '')),
-    duration INTEGER NOT NULL
+    event TEXT NOT NULL CHECK(event IN ('opened', 'closed')),
+    type TEXT CHECK(type IN ('auto', 'manual'))
 );
 
-CREATE INDEX idx_timestamp ON tickets(timestamp);
+CREATE TABLE IF NOT EXISTS clear (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    type TEXT NOT NULL CHECK(type IN ('auto', 'manual')),
+    start INTEGER NOT NULL,
+    end INTEGER NOT NULL,
+    open_event_id INTEGER NOT NULL,
+    close_event_id INTEGER NOT NULL,
+    FOREIGN KEY(open_event_id) REFERENCES events(id),
+    FOREIGN KEY(close_event_id) REFERENCES events(id)
+);
 
-CREATE INDEX idx_status ON tickets(status);
+CREATE TABLE IF NOT EXISTS anomaly (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    type TEXT NULL,
+    timestamp INTEGER NOT NULL,
+    counterparty_event_id INTEGER NULL,
+    event_id INTEGER NOT NULL,
+    detail TEXT NOT NULL CHECK ( detail IN ('missing_close',
+                                            'missing_open',
+                                            'null_type',
+                                            'duplicate_open',
+                                            'duplicate_close',
+                                            'negative_duration',
+                                            'out_of_order') ),
+    FOREIGN KEY(counterparty_event_id) REFERENCES events(id),
+    FOREIGN KEY(event_id) REFERENCES events(id)
+);
+
+CREATE INDEX IF NOT EXISTS idx_event_timestamp ON events(timestamp);
+CREATE INDEX IF NOT EXISTS idx_action_timestamp ON events(timestamp, type);
+CREATE INDEX IF NOT EXISTS idx_event_type ON events(type);
+CREATE INDEX IF NOT EXISTS idx_event ON events(id);
+
+CREATE INDEX IF NOT EXISTS idx_clear_action_start ON clear(type, start);
+CREATE INDEX IF NOT EXISTS idx_clear_start ON clear(start);
+
+CREATE INDEX IF NOT EXISTS idx_anomaly_timestamp ON anomaly(timestamp);
+CREATE INDEX IF NOT EXISTS idx_anomaly_detail_timestamp ON anomaly(detail, timestamp);
+
+COMMIT;
